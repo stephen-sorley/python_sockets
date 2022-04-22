eeb70c93575da288295650bada6398f4f74119b0
Stephen Sorley <stephen.sorley@gmail.com>
2022-04-21 21:12:50 -0400

    Added basic echo server implementation, (everything's hardcoded)

---
+++ client.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+# client.py
+#
+# Connects to the client, and sends some dummy text to it.
+#
+
+import socket
+
+# AF_INET: use IPv4 for addresses. To use IPv6, we'd set AF_INET6 instead.
+# SOCK_STREAM: use TCP to send data. To use UDP, we'd set SOCK_DGRAM instead.
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+  # Specify what server we're connecting to (host name or address, and port),
+  # and what message we're going to send.
+  host = "localhost"
+  port = 25000
+  msg  = "Hey, anybody alive over there?"
+  
+  # Attempt to connect to the server.
+  s.connect((host, port))
+  
+  # Tell user what we're sending, then convert our message to a raw byte string.
+  print(f"Sending message to {host}, port {port!r}:\n   \"{msg}\"")
+  byte_msg = msg.encode(encoding="UTF-8")
+  
+  # If successful, send some text to the server (the 'b' means it's a byte string).
+  s.sendall(byte_msg)
+  
+  # Wait for the bytestring coming back from the server.
+  data = s.recv(256)
+  
+  # Print out the received data so the user can see it.
+  print(f"Received message from {host}, port {port!r}:\n   \"{msg}\"")
---
+++ server.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+# server.py
+#
+# Currently, this server is pretty dumb - it just echos back whatever you send it.
+#
+
+import socket
+
+# AF_INET: use IPv4 for addresses. To use IPv6, we'd set AF_INET6 instead.
+# SOCK_STREAM: use TCP to send data. To use UDP, we'd set SOCK_DGRAM instead.
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+  s.bind(("", 25000)) # Assign the socket to all network interfaces, on port 25000. ("" means any IP address, here)
+  # The port number is arbitrary - but the client will need to know it in order to connect to this server.
+  # Port numbers can be anything > 0 that fits in a 16-bit unsigned integer: [1,65535].
+  # Port numbers <1024 may be reserved for certain protocols, so avoid those. (Ex: SSH is port 22, HTTP is port 80)
+  
+  # Set up the socket for listening, use the default value for backlog (# of unaccepted connections before closure).
+  s.listen()
+  
+  # Start listening for a connection from a client, wait until we get one. Once we get one, returns
+  # a new socket that represents the connection with this client.
+  conn, addr = s.accept()
+  with conn:
+    print(f"Connected to client from {addr}")
+    
+    while True:
+      # Receive up to 256 bytes (characters) from the client, (BLOCKS)
+      data = conn.recv(256)
+      
+      # If the client has stopped sending stuff, exit the loop.
+      if not data:
+        break
+      
+      # Send the received data back to the cleient.
+      conn.sendall(data)
