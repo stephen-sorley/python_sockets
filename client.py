b8f9a17e73af8183f331dc2ec2c4e94a7b799017
Stephen Sorley <stephen.sorley@gmail.com>
2022-04-24 17:37:38 -0400

    Made client's message user-configurable, too.
    Also, cleaned up reporting of default values in help message, by
    using argparse's built-in functionality for that, instead of
    duplicating the value.

--- client.py
+++ client.py
@@ -31,10 +31,13 @@ def network_port(net_port):
 # (For example, SSH is port 22, HTTP is port 80, HTTPS is port 443)
 
 parser.add_argument("-p","--port", default=25000, type=network_port,
-                    help="port number to connect to, in range [1024,65535] (defaults to 25000)")
+                    help="port number to connect to, in range [1024,65535] (default: %(default)s)")
 
 parser.add_argument("-s","--server", default="localhost",
-                    help="IPv4 address or name of server to connect to (defaults to localhost)")
+                    help="IPv4 address or name of server to connect to (default: %(default)s)")
+
+parser.add_argument("msg", metavar="...message...", default="Hey, anybody alive over there?", nargs="?",
+                    help="message to send to the server (default: \'%(default)s\')")
 
 args = parser.parse_args()
 
@@ -47,16 +50,12 @@ args = parser.parse_args()
 # AF_INET: use IPv4 for addresses. To use IPv6, we'd set AF_INET6 instead.
 # SOCK_STREAM: use TCP to send data. To use UDP, we'd set SOCK_DGRAM instead.
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-  # Specify what server we're connecting to (host name or address, and port),
-  # and what message we're going to send.
-  msg  = "Hey, anybody alive over there?"
-  
   # Attempt to connect to the server.
   s.connect((args.server, args.port))
   
   # Tell user what we're sending, then convert our message to a raw byte string.
-  print(f"Sending message to {args.server}, port {args.port!r}:\n   \"{msg}\"")
-  byte_msg = msg.encode(encoding="UTF-8")
+  print(f"Sending message to {args.server}, port {args.port!r}:\n   \"{args.msg}\"")
+  byte_msg = args.msg.encode(encoding="UTF-8")
   
   # If successful, send some text to the server.
   s.sendall(byte_msg)
@@ -65,4 +64,5 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
   data = s.recv(256)
   
   # Print out the received data so the user can see it.
+  msg = data.decode(encoding="UTF-8")
   print(f"Received message from {args.server}, port {args.port!r}:\n   \"{msg}\"")
